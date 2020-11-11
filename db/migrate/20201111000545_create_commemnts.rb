ca45cb72b40a7de63db41b2c5971950b062086af
okamin07 <ryoya.okamura@di-v.co.jp>
2020-11-11 10:46:57 +0900

    first commit

---
+++ db/migrate/20201111000545_create_commemnts.rb
@@ -0,0 +1,10 @@
+class CreateCommemnts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :commemnts do |t|
+      t.references :user, foreign_key: true
+      t.references :prototype, foreign_key: true
+      t.text :text
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/commemnt_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommemntTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
