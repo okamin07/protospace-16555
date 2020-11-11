39f562cc4d549eed3901ab2574e05f628a40a514
okamin07 <ryoya.okamura@di-v.co.jp>
2020-11-11 20:07:30 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:position,:occupation,:profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:occupation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
   end
 end
